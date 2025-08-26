7174dbf88ac7815f2d9a1558f2a7600cbf7fdee0
Marco Sandoval <marcos019dsg@gmail.com201643020+Marco019Dg@users.noreply.github.com>
2025-08-26 04:06:16 -0600

    se actualizo el archivo ai.ts

--- src/lib/ai.ts
+++ src/lib/ai.ts
@@ -5,4 +5,3 @@ export const openrouter = createOpenRouter({
 });
 
 //Esta función sirve para crear la instancia/configuración de cliente que se conecta con la API de OpenRouter
-console.log("OPENROUTER_KEY:", import.meta.env.VITE_OPENROUTER_KEY);
